febd1e88173c08fb0e2a3980b2da482728d17302
Vladimir Motylenko <me@vldm.cc>
2020-10-28 19:03:56 +0200

    Add evm bench.

---
+++ evm-utils/evm-state/benches/bench_evm.rs
@@ -0,0 +1,154 @@
+use criterion::{Throughput, Criterion, criterion_group, criterion_main};
+
+use evm_state::layered_backend::*;
+use evm_state::*;
+use assert_matches::assert_matches;
+use evm::{backend::ApplyBackend, Capture, CreateScheme, ExitReason, ExitSucceed, Handler};
+use hex;
+use log::*;
+use primitive_types::{H160, H256, U256};
+use sha3::{Digest, Keccak256};
+use std::sync::RwLock;
+
+fn name_to_key(name: &str) -> H160 {
+    let hash = H256::from_slice(Keccak256::digest(name.as_bytes()).as_slice());
+    hash.into()
+}
+
+fn criterion_benchmark(c: &mut Criterion) {
+
+    let mut group = c.benchmark_group("Evm");
+    // group.throughput(Throughput::Elements(1 as u64));
+    // group.bench_function("create_hello", |b| {
+    //     let accounts = ["contract", "caller"];
+
+    //     let code = hex::decode(HELLO_WORLD_CODE).unwrap();
+    //     let data = hex::decode(HELLO_WORLD_ABI).unwrap();
+
+    //     let vicinity = MemoryVicinity {
+    //         gas_price: U256::zero(),
+    //         origin: H160::default(),
+    //         chain_id: U256::zero(),
+    //         block_hashes: Vec::new(),
+    //         block_number: U256::zero(),
+    //         block_coinbase: H160::default(),
+    //         block_timestamp: U256::zero(),
+    //         block_difficulty: U256::zero(),
+    //         block_gas_limit: U256::max_value(),
+    //     };
+    //     let config = evm::Config::istanbul();
+    //     let backend = EvmState::new(vicinity);
+
+    //     let backend = RwLock::new(backend);
+
+    //     let mut locked = EvmState::try_lock(&backend).unwrap();
+
+    //     {
+    //         let state = locked.fork_mut();
+
+    //         for acc in &accounts {
+    //             let account = name_to_key(acc);
+    //             let memory = AccountState {
+    //                 ..Default::default()
+    //             };
+    //             state.accounts.insert(account, memory);
+    //         }
+    //     }
+
+    //     let mut executor =
+    //         StaticExecutor::with_config(locked.backend(), config, usize::max_value());
+
+        
+    //     b.iter(|| {
+    //         let exit_reason = match executor.rent_executor().create(
+    //             name_to_key("caller"),
+    //             CreateScheme::Fixed(name_to_key("contract")),
+    //             U256::zero(),
+    //             code.clone(),
+    //             None,
+    //         ) {
+    //             Capture::Exit((s, _, v)) => (s, v),
+    //             Capture::Trap(_) => unreachable!(),
+    //         };
+    
+    //         assert_matches!(exit_reason, (ExitReason::Succeed(ExitSucceed::Returned), _));
+            
+    //     })
+    // });
+
+    group.throughput(Throughput::Elements(1 as u64));
+    // simple_logger::SimpleLogger::new().init().unwrap();
+    group.bench_function("call_hello", |b| {
+        let accounts = ["contract", "caller"];
+
+        let code = hex::decode(HELLO_WORLD_CODE).unwrap();
+        let data = hex::decode(HELLO_WORLD_ABI).unwrap();
+
+        let vicinity = MemoryVicinity {
+            gas_price: U256::zero(),
+            origin: H160::default(),
+            chain_id: U256::zero(),
+            block_hashes: Vec::new(),
+            block_number: U256::zero(),
+            block_coinbase: H160::default(),
+            block_timestamp: U256::zero(),
+            block_difficulty: U256::zero(),
+            block_gas_limit: U256::max_value(),
+        };
+        let config = evm::Config::istanbul();
+        let backend = EvmState::new(vicinity);
+
+        let backend = RwLock::new(backend);
+
+        let mut locked = EvmState::try_lock(&backend).unwrap();
+
+        {
+            let state = locked.fork_mut();
+
+            for acc in &accounts {
+                let account = name_to_key(acc);
+                let memory = AccountState {
+                    ..Default::default()
+                };
+                state.accounts.insert(account, memory);
+            }
+        }
+
+        let mut executor =
+            StaticExecutor::with_config(locked.backend(), config, usize::max_value());
+
+        let exit_reason = match executor.rent_executor().create(
+            name_to_key("caller"),
+            CreateScheme::Fixed(name_to_key("contract")),
+            U256::zero(),
+            code.clone(),
+            None,
+        ) {
+            Capture::Exit((s, _, v)) => (s, v),
+            Capture::Trap(_) => unreachable!(),
+        };
+
+        assert_matches!(exit_reason, (ExitReason::Succeed(ExitSucceed::Returned), _));
+        
+        b.iter(|| {
+            let exit_reason = executor.rent_executor().transact_call(
+                name_to_key("contract"),
+                name_to_key("contract"),
+                U256::zero(),
+                data.to_vec(),
+                usize::max_value(),
+            );
+    
+            let result = hex::decode(HELLO_WORLD_RESULT).unwrap();
+            match exit_reason {
+                (ExitReason::Succeed(ExitSucceed::Returned), res) if res == result => {}
+                any_other => panic!("Not expected result={:?}", any_other),
+            }
+   
+        });
+    });
+    group.finish();
+}
+
+criterion_group!(benches, criterion_benchmark);
+criterion_main!(benches);
