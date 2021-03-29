bbeda2dfff32618defc5a63ea0fe700ae4099b7a
hrls <viktor.kharitonovich@gmail.com>
2021-03-29 12:10:47 +0300

    chore: remove explorer and web3

--- web3.js/test/fixtures/noop-rust/build.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-set -ex
-
-cd "$(dirname "$0")"
-
-../../../examples/bpf-rust-noop/do.sh build
-cp ../../../examples/bpf-rust-noop/target/bpfel-unknown-unknown/release/solana_bpf_rust_noop.so .
