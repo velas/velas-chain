449b91a0817a335a836b0369c20117dfe3fa9e97
Vladimir Motylenko <me@vldm.cc>
2021-04-19 12:36:37 +0300

    fix(url): Fixup default url to mainnet.

--- cli-config/src/config.rs
+++ cli-config/src/config.rs
@@ -30,7 +30,7 @@ impl Default for Config {
             keypair_path.extend(&[".config", "velas", "id.json"]);
             keypair_path.to_str().unwrap().to_string()
         };
-        let json_rpc_url = "https://testnet.velas.com/rpc".to_string();
+        let json_rpc_url = "https://mainnet.velas.com/rpc".to_string();
 
         // Empty websocket_url string indicates the client should
         // `Config::compute_websocket_url(&json_rpc_url)`
