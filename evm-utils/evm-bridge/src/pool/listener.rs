b3b27e83c5fa5b4d4748c4687b93b551abb6b8b0
Vladimir Motylenko <me@vldm.cc>
2021-10-18 15:29:10 +0300

    fix(evm-bridge): Fix sender error handling.

--- evm-utils/evm-bridge/src/pool/listener.rs
+++ evm-utils/evm-bridge/src/pool/listener.rs
@@ -1,25 +1,50 @@
+use log::*;
 use std::fmt::{Debug, LowerHex};
 use std::sync::Arc;
-
-use log::*;
+use tokio::runtime::Handle;
 use txpool::Listener;
 
 use super::PooledTransaction;
 
 pub struct PoolListener;
+impl PoolListener {
+    fn notify_tx_removed(&self, tx: &Arc<PooledTransaction>) {
+        if let Ok(handle) = Handle::try_current() {
+            let tx = tx.clone();
+            handle.spawn(async move {
+                if let Err(e) = tx
+                    .hash_sender
+                    .send(Err(evm_rpc::Error::TransactionRemoved {}))
+                    .await
+                {
+                    warn!(
+                        "PoolListener failed to notify tx sender about transaction, error:{:?}",
+                        e
+                    )
+                }
+            });
+        } else if let Err(e) = tx
+                .hash_sender
+                .blocking_send(Err(evm_rpc::Error::TransactionRemoved {}))
+        {
+            warn!(
+                "PoolListener failed to notify tx sender about transaction, error:{:?}",
+                e
+            )
+        }
+    }
+}
 
 impl Listener<PooledTransaction> for PoolListener {
     fn added(&mut self, tx: &Arc<PooledTransaction>, old: Option<&Arc<PooledTransaction>>) {
-        trace!("PoolListener::added: tx = {:?}, old = {:?}", tx, old);
+        debug!("PoolListener::added: tx = {:?}, old = {:?}", tx, old);
 
         if let Some(old) = old {
             info!(
                 "Transaction {} replaced with transaction {}",
                 old.hash, tx.hash
             );
-            old.hash_sender
-                .blocking_send(Err(evm_rpc::Error::TransactionReplaced {}))
-                .unwrap();
+            self.notify_tx_removed(old)
         }
     }
 
@@ -28,132 +53,30 @@ impl Listener<PooledTransaction> for PoolListener {
         tx: &Arc<PooledTransaction>,
         reason: &txpool::Error<H>,
     ) {
-        trace!(
+        debug!(
             "PoolListener::rejected: tx = {:?}, reason = {:?}",
-            tx,
-            reason
+            tx, reason
         );
+        self.notify_tx_removed(tx)
     }
 
     fn dropped(&mut self, tx: &Arc<PooledTransaction>, by: Option<&PooledTransaction>) {
-        trace!("PoolListener::dropped: tx = {:?}, by = {:?}", tx, by);
+        debug!("PoolListener::dropped: tx = {:?}, by = {:?}", tx, by);
+        self.notify_tx_removed(tx)
     }
 
     fn invalid(&mut self, tx: &Arc<PooledTransaction>) {
-        trace!("PoolListener::invalid: tx = {:?}", tx);
+        debug!("PoolListener::invalid: tx = {:?}", tx);
+        self.notify_tx_removed(tx)
     }
 
     fn canceled(&mut self, tx: &Arc<PooledTransaction>) {
-        trace!("PoolListener::canceled: tx = {:?}", tx);
+        debug!("PoolListener::canceled: tx = {:?}", tx);
+        self.notify_tx_removed(tx)
     }
 
     fn culled(&mut self, tx: &Arc<PooledTransaction>) {
-        trace!("PoolListener::culled: tx = {:?}", tx);
+        debug!("PoolListener::culled: tx = {:?}", tx);
+        self.notify_tx_removed(tx)
     }
 }
-
-// pool.import(test_tx(1, 42, "11", &SK1)).unwrap();
-// pool.import(test_tx(1, 9000, "11", &SK1)).unwrap();
-
-// PoolListener::added:
-//   tx = PooledTransaction {
-//     inner: Transaction {
-//       nonce: 1,
-//       gas_price: 42,
-//       gas_limit: 30000000,
-//       action: Create,
-//       value: 0,
-//       signature: TransactionSignature {
-//         v: 257,
-//         r: 0x2e677f66ca9da7276c9c22419daacec43bef4e09d6e0c7a52618823f9cc36fe1,
-//         s: 0x76e5d225aa8855a095daa5e0d4b24ac3912ca7f73e83a6d9adb5989e5b5229af
-//       },
-//       input: [49, 49]
-//     },
-//     meta_keys: {},
-//     sender: 0x1a642f0e3c3af545e7acbd38b07251b3990914f1,
-//     hash: 0x3c44e9dff342d80ba6cfed312772e21cc655eecbfbf0fc438e094745325e4c81,
-//     hash_sender: Sender {
-//       chan: Tx {
-//         inner: Chan {
-//           tx: Tx {
-//             block_tail: 0x7ff24703d000,
-//             tail_position: 0
-//           },
-//           semaphore: (Semaphore { permits: 1 }, 1),
-//           rx_waker: AtomicWaker,
-//           tx_count: 1,
-//           rx_fields: "..."
-//         }
-//       }
-//     }
-//   },
-//   old = None
-//
-// PoolListener::added:
-//   tx = PooledTransaction {
-//     inner: Transaction {
-//       nonce: 1,
-//       gas_price: 9000,
-//       gas_limit: 30000000,
-//       action: Create,
-//       value: 0,
-//       signature: TransactionSignature {
-//         v: 257,
-//         r: 0x5f0e66124325cf61419ec0c3eb7bb7889c13de3f8c04fd20ef0ad2655961bb1b,
-//         s: 0x2290a1d90070b84a4a4961d4756967250574cd584fd3f2308311a88442a5bc1f
-//       },
-//       input: [49, 49]
-//     },
-//     meta_keys: {},
-//     sender: 0x1a642f0e3c3af545e7acbd38b07251b3990914f1,
-//     hash: 0x17f17989730b26bba8486a78da69e834fe0a881a1e0eda4e452f83742e8b5555,
-//     hash_sender: Sender {
-//       chan: Tx {
-//         inner: Chan {
-//           tx: Tx {
-//             block_tail: 0x7ff24703e000,
-//             tail_position: 0
-//           },
-//           semaphore: (Semaphore { permits: 1 }, 1),
-//           rx_waker: AtomicWaker,
-//           tx_count: 1,
-//           rx_fields: "..."
-//         }
-//       }
-//     }
-//   },
-//   old = Some(
-//     PooledTransaction {
-//       inner: Transaction {
-//         nonce: 1,
-//         gas_price: 42,
-//         gas_limit: 30000000,
-//         action: Create,
-//         value: 0,
-//         signature: TransactionSignature {
-//           v: 257,
-//           r: 0x2e677f66ca9da7276c9c22419daacec43bef4e09d6e0c7a52618823f9cc36fe1,
-//           s: 0x76e5d225aa8855a095daa5e0d4b24ac3912ca7f73e83a6d9adb5989e5b5229af
-//         },
-//         input: [49, 49]
-//       },
-//       meta_keys: {},
-//       sender: 0x1a642f0e3c3af545e7acbd38b07251b3990914f1,
-//       hash: 0x3c44e9dff342d80ba6cfed312772e21cc655eecbfbf0fc438e094745325e4c81,
-//       hash_sender: Sender {
-//         chan: Tx {
-//           inner: Chan {
-//             tx: Tx {
-//               block_tail: 0x7ff24703d000,
-//               tail_position: 0
-//             },
-//             semaphore: (Semaphore { permits: 1 }, 1),
-//             rx_waker: AtomicWaker,
-//             tx_count: 1,
-//             rx_fields: "..."
-//           }
-//         }
-//       }
-//     }
-//   )
