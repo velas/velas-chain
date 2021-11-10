711cf430c3a508c131374e734f28fb54294a609d
[ G'Lab ] Team <92644489+g3n3s1sl4b@users.noreply.github.com>
2021-11-10 13:43:26 +0100

    fix : awk syntax error near line 1

--- scripts/system-stats.sh
+++ scripts/system-stats.sh
@@ -18,7 +18,7 @@ while true; do
   # collect the total ram usage by dividing used memory / total memory
   ram_total_and_usage=$(echo "${top_ouput}" | grep '.*B Mem'| tail -1 | sed "s/.*: *\([0-9.]*\)%* total.*, *\([0-9.]*\)%* used.*/\1 \2/")
   read -r total used <<< "$ram_total_and_usage"
-  ram_usage=$(awk "BEGIN {print $used / $total * 100}")
+  ram_usage=$(awk 'BEGIN {print $used / $total * 100}')
   cpu_report="cpu_usage=$cpu_usage,ram_usage=$ram_usage"
 
   # if nvidia-smi exists, report gpu stats
@@ -31,11 +31,11 @@ while true; do
     for entry in "${individual_gpu_usage[@]}"
     do
       read -r compute mem_used mem_total <<< "${entry//,/}"
-      total_gpu_usage=$(awk "BEGIN {print $total_gpu_usage + $compute }")
-      total_gpu_mem_usage=$(awk "BEGIN {print $total_gpu_mem_usage + $mem_used / $mem_total * 100}")
+      total_gpu_usage=$(awk 'BEGIN {print $total_gpu_usage + $compute }')
+      total_gpu_mem_usage=$(awk 'BEGIN {print $total_gpu_mem_usage + $mem_used / $mem_total * 100}')
     done
-    avg_gpu_usage=$(awk "BEGIN {print $total_gpu_usage / $num_gpus}")
-    avg_gpu_mem_usage=$(awk "BEGIN {print $total_gpu_mem_usage / $num_gpus}")
+    avg_gpu_usage=$(awk 'BEGIN {print $total_gpu_usage / $num_gpus}')
+    avg_gpu_mem_usage=$(awk 'BEGIN {print $total_gpu_mem_usage / $num_gpus}')
     gpu_report=",avg_gpu_usage=$avg_gpu_usage,avg_gpu_mem_usage=$avg_gpu_mem_usage"
   fi
 
