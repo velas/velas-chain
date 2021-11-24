ecccb346a16915d991dd4e49ab382345cae4630e
Maksim Vykhota <m.vykhota@protonmail.com>
2021-11-24 14:07:38 +0200

    fix(velas-chain): Downgrade Rust toolchain to 1.54

--- ci/rust-version.sh
+++ ci/rust-version.sh
@@ -18,7 +18,7 @@
 if [[ -n $RUST_STABLE_VERSION ]]; then
   stable_version="$RUST_STABLE_VERSION"
 else
-  stable_version=1.55.0
+  stable_version=1.54.0
 fi
 
 if [[ -n $RUST_NIGHTLY_VERSION ]]; then
