3d81a8f5ecdda84addabe466b111de65223d6a25
Maksim Vykhota <m.vykhota@protonmail.com>
2022-06-08 14:09:16 +0300

    split `repeat` to tasks

--- evm-utils/evm-block-recovery/src/routines/repeat.rs
+++ evm-utils/evm-block-recovery/src/routines/repeat.rs
@@ -1,5 +1,13 @@
-use anyhow::*;
+use anyhow::{Context, Result};
+use evm_state::Block;
 use solana_storage_bigtable::LedgerStorage;
+use tokio::sync::mpsc;
+
+#[derive(Debug)]
+struct BlockMessage<B> {
+    idx: usize,
+    block: B,
+}
 
 pub async fn repeat_evm(
     block_number: u64,
@@ -18,12 +26,49 @@ pub async fn repeat_evm(
         )
     }
 
-    for (idx, block_number) in (block_number..block_number + limit).enumerate() {
-        log::info!("[{}] Repeating block {}", idx, block_number);
-        // TODO: implement meaningful display printing
-        // log::info!("[{}] Source Ledger: {:?}", idx, &src);
-        // log::info!("[{}] Destination Ledger: {:?}", idx, &dst);
+    let (sender, mut receiver) = mpsc::unbounded_channel::<BlockMessage<Block>>();
+    let writer = tokio::spawn(async move {
+        log::info!("Writer task started");
 
+        while let Some(message) = receiver.recv().await {
+            log::info!(
+                "[{}] Uploading block {} to the Destination Ledger",
+                message.idx,
+                block_number
+            );
+
+            let uploaded = dst
+                .upload_evm_block(block_number, message.block)
+                .await
+                .context(format!(
+                    "Unable to upload block {} to the Destination Ledger",
+                    block_number
+                ));
+
+            match uploaded {
+                Ok(()) => {
+                    log::info!(
+                        "[{}] Block {} uploaded successfully",
+                        message.idx,
+                        block_number
+                    )
+                }
+                Err(_) => {
+                    log::error!(
+                        "[{}] Failed to upload block {} to the Destination Ledger",
+                        message.idx,
+                        block_number
+                    )
+                }
+            }
+
+            uploaded.unwrap(); // NOTICE: early return on error
+        }
+
+        log::info!("Writer task ended");
+    });
+
+    for (idx, block_number) in (block_number..block_number + limit).enumerate() {
         log::info!(
             "[{}] Reading block {} from the Source Ledger",
             idx,
@@ -37,31 +82,28 @@ pub async fn repeat_evm(
                 block_number
             ))?;
 
-        // TODO: add additional checks
-        // TODO: log and display context details in case of early return
-        log::info!(
-            "[{}] Uploading block {} to the Destination Ledger",
-            idx,
-            block_number
-        );
-        dst.upload_evm_block(block_number, block)
-            .await
-            .context(format!(
-                "Unable to read block {} from the Source Ledger",
-                block_number
-            ))?;
+        sender.send(BlockMessage { idx, block })?;
     }
+
+    drop(sender);
+
+    let _result1 = writer.await;
 
     Ok(())
 }
 
 pub async fn repeat_native(
-    _block_number: u64,
+    block_number: u64,
     _limit: u64,
-    _src: LedgerStorage,
-    _dst: LedgerStorage,
+    src: LedgerStorage,
+    dst: LedgerStorage,
 ) -> Result<()> {
     log::info!("TODO: `repeat-native` command");
+
+    let block = src.get_confirmed_block(block_number).await.unwrap();
+    dst.upload_confirmed_block(block_number, block)
+        .await
+        .unwrap();
 
     Ok(())
 }
