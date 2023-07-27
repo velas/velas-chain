7c55fa4f635483c03300a45a3e962b06e6b09636
Anton Rubets <anton.rubets@dysnix.com>
2023-07-27 12:39:01 +0300

    Fix pipeline

--- ci/github-check-nightly-release.sh
+++ ci/github-check-nightly-release.sh
@@ -16,7 +16,8 @@ our_commit=$(git rev-parse HEAD)
 if [ -z "$our_commit" ]; then
     exit 1
 fi
-hub release
+
+env HUB_VERBOSE=1 hub release
 echo $GITHUB_TOKEN
 echo ${title} > release.md
 echo "" >> release.md
