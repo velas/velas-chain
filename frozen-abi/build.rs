e357ac818d58a902c56f6c519583039ee6fa4be2
Maksim Vykhota <m.vykhota@protonmail.com>
2024-01-16 18:28:06 +0300

    add note to improve

--- frozen-abi/build.rs
+++ frozen-abi/build.rs
@@ -1,6 +1,8 @@
 extern crate rustc_version;
 use rustc_version::{version_meta, Channel};
 
+// add stuff
+
 fn main() {
     // Copied and adapted from
     // https://github.com/Kimundi/rustc-version-rs/blob/1d692a965f4e48a8cb72e82cda953107c0d22f47/README.md#example
